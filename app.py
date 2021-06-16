cd38a48c9be98a636b8e14e1e1f804012f2f0545
J.Shin <bonanob@hotmail.com>
2021-06-16 09:30:31 +0200

    Change line to  candlestick plot, add MA's, volume

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-# Long Short Term Memory LSTM to predict the closing stock prices
+# TODO: Long Short Term Memory LSTM to predict the closing stock prices
 
 # Import the libraries
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
-import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import dash_bootstrap_components as dbc
-import pandas_datareader as pdr
 import pandas as pd
 import datetime as dt
+import yfinance as yf
 
 # import numpy as np
 # import math
@@ -21,60 +22,51 @@ import datetime as dt
 
 # Yesterday because today's market might not be closed.
 today = dt.datetime.now()
-yesterday = (today - dt.timedelta(days=1)).strftime('%Y-%m-%d')
-
-# df = pdr.DataReader('AAPL', data_source='yahoo', start='2015-01-01', end=yesterday)
-# print(df.head())
-
-#
-# print(df.head())
-# print('rows, columns', df.shape)
-# print(df[df.index > today - dt.timedelta(days=121)].describe())
+yesterday = (today - dt.timedelta(days=1)).strftime("%Y-%m-%d")
 
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX, "style.css"],
                 # Mobile responsive
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}]
+                meta_tags=[{"name": "viewport",
+                            "content": "width=device-width, initial-scale=1.0"}]
                 )
 
 app.layout = dbc.Container([
 
     dbc.Row(
         dbc.Col(
-            html.H1('Stock Price Trend',
-                    className='text-center text-primary',
-                    style={"margin":"5%"}),
-        width=12),
-    align="center"),
+            html.H1("Stock Price Trend",
+                    className="text-center text-primary",
+                    style={"margin": "3%"}),
+            width=12),
+        align="center"),
 
     dbc.Row([
         dbc.Col(
             html.H5("Stock Symbol",
-                        style={"margin":"3%", "text-align":"center"}),
+                    style={"margin": "3%", "text-align": "center"}),
             width=3),
 
         dbc.Col(
             dbc.Input(
-                id="input1",
+                id="symbol",
                 type="text",
                 placeholder="",
-                debounce=True,
-                value="^GDAXI",
-                bs_size="sm"),
+                bs_size="sm",
+                debounce=True),
             width=4),
 
         dbc.Col(
             dcc.DatePickerRange(
-            id='my-date-picker-range',
-            min_date_allowed=dt.date(2000,1,1),
-            max_date_allowed=yesterday,
-            initial_visible_month=yesterday,
-            end_date=yesterday,
-            className="date_range"),
-        width={"size":4, "offset":1}),
-        # html.Div(id='output-container-date-picker-range')
-    ], justify="left"),
+                id="date_range",
+                display_format='DD-MM-YYYY',
+                min_date_allowed=dt.date(2000, 1, 1),
+                max_date_allowed=yesterday,
+                initial_visible_month=yesterday,
+                start_date=dt.date(2021, 1, 1),
+                end_date=yesterday),
+            width={"size": 4, "offset": 1}),
+    ], justify="left", style={"margin":"5px 0px"}),
 
     dbc.Row([
         dbc.Col([
@@ -87,23 +79,79 @@ app.layout = dbc.Container([
 
 # Callback section: connecting the components
 # ************************************************************************
-# Line chart - Single
-@app.callback(
-    Output('line-fig', 'figure'),
-    [Input('input1', 'value'),
-     Input('my-date-picker-range', 'start_date'),
-     Input('my-date-picker-range', 'end_date')]
+=@app.callback(
+    Output("line-fig", "figure"),
+    [Input("symbol", "value"),
+     Input("date_range", "start_date"),
+     Input("date_range", "end_date")]
 )
-def update_graph(stock_slctd, start_date, end_date ):
-    print(stock_slctd)
-    print(type(stock_slctd))
-    # Get the stock quote
-    # TODO: Users input 1.stock_symbol 2.start/end dates
-    df = pdr.DataReader(stock_slctd, data_source='yahoo', start=start_date, end=end_date)
-    print(df.head())
-    figln = px.line(df, y='Close')
-    return figln
+def update_graph(symbol, start_date, end_date):
+    # Set initial ticker symbol
+    if symbol is None:
+        yf_symbol = yf.Ticker("^GDAXI")
+    else:
+        yf_symbol = yf.Ticker(symbol)
+
+
+    # .info["shortName"] for the name of the company
+    info = yf_symbol.info
+
+    # .history for the historical data
+    hist = yf_symbol.history(start=start_date, end=end_date)
+
+    # Moving average
+    hist["ma_5"] = hist["Close"].rolling(window=5, min_periods=0).mean()
+    hist["ma_20"] = hist["Close"].rolling(window=20, min_periods=0).mean()
+    hist["ma_60"] = hist["Close"].rolling(window=60, min_periods=0).mean()
+    hist["ma_120"] = hist["Close"].rolling(window=120, min_periods=0).mean()
+
+    # To make two plots that share x axis
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.03, row_width=[0.2, 0.7])
+
+    # .add_trace for adding plots to existing plot
+    fig.add_trace(
+        go.Candlestick(x=hist.index, open=hist['Open'], high=hist['High'], low=hist['Low'], close=hist['Close'],
+                       name="OHLC"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=hist.index, y=hist["ma_5"], marker_color="purple", name="MA5"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=hist.index, y=hist["ma_20"], marker_color="orange", name="MA20"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=hist.index, y=hist["ma_60"], marker_color="cyan", name="MA60"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=hist.index, y=hist["ma_120"], marker_color="pink", name="MA120"), row=1, col=1)
+    fig.add_trace(go.Bar(x=hist.index, y=hist["Volume"], marker_color="black", showlegend=False), row=2, col=1)
+
+    # plot styling
+    fig.update_layout(
+        margin=dict(l=80, r=80, t=80, b=80),
+        title={'text': info["shortName"], 'font': {'size': 16}, 'x': 0.08, 'y': 0.93, 'xanchor': 'left'},
+        xaxis_tickfont_size=12,
+        yaxis=dict(
+            title='Price',
+            titlefont_size=14,
+            tickfont_size=12
+        ),
+        autosize=False,
+        height=800,
+    )
+
+    # Remove rangeslider
+    fig.update(layout_xaxis_rangeslider_visible=False)
+
+
+    return fig
 
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8000)
+
+    # token = "Tpk_b880885bcfdb4be2aa9eb43c6455eabb"
+    #
+    # api_url = f"https://sandbox.iexapis.com/stable/stock/aapl/quote/?token={token}"
+    #
+    # data = requests.get(api_url).json()
+    # print(data)
+    #
+    # api_url1 = f"https://sandbox.iexapis.com/stable/search/apple/?token={token}"
+    # data1 = requests.get(api_url1).json()
+    # print(data1)
+
+    # df = pd.read_json(data[0])
+    # print(df)
